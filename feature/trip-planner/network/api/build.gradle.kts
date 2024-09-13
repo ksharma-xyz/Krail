ea91603babb18edd81d459c9162cbb5b3ed5580d
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-13 20:54:53 +1000

    Create trip-planner network module (#41)

---
+++ feature/trip-planner/network/api/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.krail.android.library)
+}
+
+android {
+    namespace = "xyz.ksharma.krail.trip_planner.network.api"
+}
+
+dependencies {
+    api(projects.core.network)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+}
---
+++ feature/trip-planner/network/real/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.krail.android.library)
+    alias(libs.plugins.krail.android.hilt)
+}
+
+android {
+    namespace = "xyz.ksharma.krail.trip_planner.network.real"
+}
+
+dependencies {
+    implementation(projects.core.coroutinesExt)
+    implementation(projects.feature.tripPlanner.network.api)
+
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+}
