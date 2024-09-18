8c42fd2b8c2b47a064e49797deba8b0e84acba5f
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-19 06:30:25 +1000

    Update TripPlanningService

--- app/src/main/java/xyz/ksharma/krail/MainActivity.kt
+++ app/src/main/java/xyz/ksharma/krail/MainActivity.kt
@@ -11,6 +11,7 @@ import timber.log.Timber
 import xyz.ksharma.krail.design.system.theme.StartTheme
 import xyz.ksharma.krail.trip_planner.network.api.model.StopType
 import xyz.ksharma.krail.trip_planner.network.api.repository.TripPlanningRepository
+import xyz.ksharma.krail.trip_planner.network.api.service.TripPlanningService
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -19,11 +20,13 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var tripPlanningRepo: TripPlanningRepository
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Configures the system bars with a transparent background.
         enableEdgeToEdge()
+
 
         lifecycleScope.launch {
             var x =
@@ -37,6 +40,7 @@ class MainActivity : ComponentActivity() {
             x = tripPlanningRepo.stopFinder(stopType = StopType.ANY, stopSearchQuery = "Central")
             Timber.d("ANY: ${x.getOrNull()?.locations?.map { it.productClasses.contains(1) }}")
         }
+
 
         setContent {
             StartTheme {
