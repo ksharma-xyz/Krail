af733064cff64b86d0a3e5841f6c1d091ef6e11a
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-19 06:43:33 +1000

    Fix Dagger use object for Provides

--- feature/trip-planner/network/api/src/main/kotlin/xyz/ksharma/krail/trip_planner/network/api/di/ServiceModule.kt
+++ feature/trip-planner/network/api/src/main/kotlin/xyz/ksharma/krail/trip_planner/network/api/di/ServiceModule.kt
@@ -5,17 +5,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
-import xyz.ksharma.krail.network.di.NetworkModule
 import xyz.ksharma.krail.trip_planner.network.api.service.TripPlanningService
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface ServiceModule {
+object ServiceModule {
 
     @Provides
     @Singleton
     fun provideTripPlanningService(retrofit: Retrofit): TripPlanningService {
-        return NetworkModule.service(retrofit, TripPlanningService::class.java)
+        return retrofit.create(TripPlanningService::class.java)
     }
 }
