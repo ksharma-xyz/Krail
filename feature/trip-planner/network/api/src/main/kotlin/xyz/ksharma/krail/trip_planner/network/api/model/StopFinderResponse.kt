19e564db9d7e878c638839bad1867973339957ff
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-19 18:29:07 +1000

    Add TripResponse

--- feature/trip-planner/network/api/src/main/kotlin/xyz/ksharma/krail/trip_planner/network/api/model/StopFinderResponse.kt
+++ feature/trip-planner/network/api/src/main/kotlin/xyz/ksharma/krail/trip_planner/network/api/model/StopFinderResponse.kt
@@ -9,92 +9,92 @@ data class StopFinderResponse(
     val version: String,
 
     @SerialName("locations")
-    val locations: List<Location>
-)
+    val locations: List<Location>,
+) {
+    @Serializable
+    data class Location(
+        @SerialName("id")
+        val id: String,
 
-@Serializable
-data class Location(
-    @SerialName("id")
-    val id: String,
+        @SerialName("isGlobalId")
+        val isGlobalId: Boolean,
 
-    @SerialName("isGlobalId")
-    val isGlobalId: Boolean,
+        @SerialName("name")
+        val name: String,
 
-    @SerialName("name")
-    val name: String,
+        @SerialName("disassembledName")
+        val disassembledName: String? = null,
 
-    @SerialName("disassembledName")
-    val disassembledName: String? = null,
+        @SerialName("coord")
+        val coord: List<Double>,
 
-    @SerialName("coord")
-    val coord: List<Double>,
+        @SerialName("type")
+        val type: String,
 
-    @SerialName("type")
-    val type: String,
+        @SerialName("matchQuality")
+        val matchQuality: Int,
 
-    @SerialName("matchQuality")
-    val matchQuality: Int,
+        @SerialName("isBest")
+        val isBest: Boolean,
 
-    @SerialName("isBest")
-    val isBest: Boolean,
+        @SerialName("parent")
+        val parent: Parent? = null,
 
-    @SerialName("parent")
-    val parent: Parent? = null,
+        @SerialName("assignedStops")
+        val assignedStops: List<AssignedStop>? = null,
 
-    @SerialName("assignedStops")
-    val assignedStops: List<AssignedStop>? = null,
+        @SerialName("properties")
+        val properties: Properties? = null,
 
-    @SerialName("properties")
-    val properties: Properties? = null,
+        @SerialName("productClasses")
+        val productClasses: List<Int>,
+    )
 
-    @SerialName("productClasses")
-    val productClasses: List<Int>,
-)
+    @Serializable
+    data class Parent(
+        @SerialName("id")
+        val id: String? = null,
 
-@Serializable
-data class Parent(
-    @SerialName("id")
-    val id: String? = null,
+        @SerialName("name")
+        val name: String,
 
-    @SerialName("name")
-    val name: String,
+        @SerialName("type")
+        val type: String,
+    )
 
-    @SerialName("type")
-    val type: String
-)
+    @Serializable
+    data class AssignedStop(
+        @SerialName("id")
+        val id: String,
 
-@Serializable
-data class AssignedStop(
-    @SerialName("id")
-    val id: String,
+        @SerialName("isGlobalId")
+        val isGlobalId: Boolean,
 
-    @SerialName("isGlobalId")
-    val isGlobalId: Boolean,
+        @SerialName("name")
+        val name: String,
 
-    @SerialName("name")
-    val name: String,
+        @SerialName("type")
+        val type: String,
 
-    @SerialName("type")
-    val type: String,
+        @SerialName("coord")
+        val coord: List<Double>,
 
-    @SerialName("coord")
-    val coord: List<Double>,
+        @SerialName("parent")
+        val parent: Parent,
 
-    @SerialName("parent")
-    val parent: Parent,
+        @SerialName("productClasses")
+        val productClasses: List<Int>,
 
-    @SerialName("productClasses")
-    val productClasses: List<Int>,
+        @SerialName("connectingMode")
+        val connectingMode: Int,
 
-    @SerialName("connectingMode")
-    val connectingMode: Int,
+        @SerialName("properties")
+        val properties: Properties,
+    )
 
-    @SerialName("properties")
-    val properties: Properties
-)
-
-@Serializable
-data class Properties(
-    @SerialName("stopId")
-    val stopId: String
-)
+    @Serializable
+    data class Properties(
+        @SerialName("stopId")
+        val stopId: String,
+    )
+}
