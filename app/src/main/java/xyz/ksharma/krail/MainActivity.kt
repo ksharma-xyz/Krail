20a1e2ef15195d89f86ec036f534abc27b8e8aa1
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-20 20:57:46 +1000

    Log departure infor

--- app/src/main/java/xyz/ksharma/krail/MainActivity.kt
+++ app/src/main/java/xyz/ksharma/krail/MainActivity.kt
@@ -11,6 +11,9 @@ import timber.log.Timber
 import xyz.ksharma.krail.design.system.theme.StartTheme
 import xyz.ksharma.krail.trip_planner.network.api.model.StopType
 import xyz.ksharma.krail.trip_planner.network.api.repository.TripPlanningRepository
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,13 +29,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         lifecycleScope.launch {
-            var x =
+            var x = tripPlanningRepo.stopFinder(
+                stopType = StopType.ANY,
+                stopSearchQuery = "Seven Hills"
+            )
+            Timber.d("Seven Hills: ${x.getOrNull()?.locations?.map { it.productClasses.contains(1) }}")
+            x =
                 tripPlanningRepo.stopFinder(stopType = StopType.ANY, stopSearchQuery = "Central")
-            Timber.d("ANY: ${x.getOrNull()?.locations?.map { it.productClasses.contains(1) }}")
+            Timber.d("Central: ${x.getOrNull()?.locations?.map { it.productClasses.contains(1) }}")
+            x =
+                tripPlanningRepo.stopFinder(stopType = StopType.ANY, stopSearchQuery = "Townhall")
+            Timber.d("Townhall: ${x.getOrNull()?.locations?.map { it.productClasses.contains(1) }}")
+            x =
+                tripPlanningRepo.stopFinder(stopType = StopType.ANY, stopSearchQuery = "Rockdale")
+            Timber.d("Rockdale: ${x.getOrNull()?.locations?.map { it.productClasses.contains(1) }}")
+
 
             var tripResponse = tripPlanningRepo.trip()
             tripResponse.onSuccess { trip ->
                 Timber.d("Journeys: ${trip.journeys.size}")
+
+                trip.journeys.map {
+                    it.legs.forEach {
+                        Timber.d(
+                            "departureTimeEstimated: ${
+                                it.origin.departureTimeEstimated?.utcToAEST()?.formatTo12HourTime()
+                            }," +
+                                    " Destination: ${
+                                        it.destination.arrivalTimeEstimated?.utcToAEST()
+                                            ?.formatTo12HourTime()
+                                    }, " +
+                                    "Duration: ${it.duration}, " +
+                                    "transportation: ${it.transportation}",
+                        )
+                    }
+                }
+
             }.onFailure {
                 Timber.e("error: ${it.message}")
             }
@@ -43,5 +75,26 @@ class MainActivity : ComponentActivity() {
                 KrailApp()
             }
         }
+    }
+
+    private fun String.utcToAEST(): String {
+        // Parse the string as a ZonedDateTime in UTC
+        val utcDateTime = ZonedDateTime.parse(this, DateTimeFormatter.ISO_ZONED_DATE_TIME)
+
+        // Convert to AEST time zone (UTC+10)
+        val aestZoneId = ZoneId.of("Australia/Sydney")
+        val aestDateTime = utcDateTime.withZoneSameInstant(aestZoneId)
+        return aestDateTime.format(DateTimeFormatter.ISO_DATE_TIME)
+    }
+
+    fun String.formatTo12HourTime(): String {
+        // Parse the string as ZonedDateTime
+        val zonedDateTime = ZonedDateTime.parse(this)
+
+        // Define the formatter for 12-hour time with AM/PM
+        val timeFormatter = DateTimeFormatter.ofPattern("h:mm a")
+
+        // Format the ZonedDateTime to 12-hour format
+        return zonedDateTime.format(timeFormatter)
     }
 }
