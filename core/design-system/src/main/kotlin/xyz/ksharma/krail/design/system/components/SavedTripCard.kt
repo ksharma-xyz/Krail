649e811d3a12aeee6d2e1ef9efd6c561b2eaf149
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-29 07:22:29 +1000

    UI: Add SavedTripCard component (#79)

---
+++ core/design-system/src/main/kotlin/xyz/ksharma/krail/design/system/components/SavedTripCard.kt
@@ -0,0 +1,95 @@
+package xyz.ksharma.krail.design.system.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import xyz.ksharma.krail.design.system.components.foundation.Text
+import xyz.ksharma.krail.design.system.model.TransportModeType
+import xyz.ksharma.krail.design.system.preview.ComponentPreviews
+import xyz.ksharma.krail.design.system.theme.KrailTheme
+import xyz.ksharma.krail.design.system.R as DSR
+
+@Composable
+fun SavedTripCard(
+    transportModeType: TransportModeType,
+    origin: String,
+    destination: String,
+    modifier: Modifier = Modifier,
+    onStarClick: () -> Unit = {},
+    onCardClick: () -> Unit = {},
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(color = KrailTheme.colors.secondaryContainer)
+            .clickable(role = Role.Button, onClickLabel = "Open Trip Details", onClick = onCardClick)
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        TransportModeIcon(
+            transportModeType = transportModeType,
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .weight(1f)
+        ) {
+            Text(text = origin, style = KrailTheme.typography.bodyMedium)
+            Text(text = destination, style = KrailTheme.typography.bodyMedium)
+        }
+
+        Box(
+            modifier = Modifier
+                .size(32.dp)
+                .clip(CircleShape)
+                .clickable(
+                    onClickLabel = "Remove Saved Trip",
+                    role = Role.RadioButton,
+                    onClick = onStarClick,
+                )
+                .semantics(mergeDescendants = true) {},
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(DSR.drawable.star),
+                contentDescription = "Save Trip",
+                colorFilter = ColorFilter.tint(KrailTheme.colors.secondary),
+            )
+        }
+    }
+}
+
+// region Previews
+
+@ComponentPreviews
+@Composable
+private fun SavedTripCardPreview() {
+    KrailTheme {
+        SavedTripCard(
+            transportModeType = TransportModeType.Train,
+            origin = "Edmondson Park Station",
+            destination = "Harris Park Station",
+        )
+    }
+}
+
+// endregion
