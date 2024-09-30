3db903b3f8a7874e73cc6948fc2f897a5ae84db2
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-09-30 15:27:13 +1000

    UI: Add BasicJourneyCard

---
+++ core/design-system/src/main/kotlin/xyz/ksharma/krail/design/system/components/BasicJourneyCard.kt
@@ -0,0 +1,48 @@
+package xyz.ksharma.krail.design.system.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import xyz.ksharma.krail.design.system.preview.ComponentPreviews
+import xyz.ksharma.krail.design.system.theme.KrailTheme
+
+@Composable
+internal fun BasicJourneyCard(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = KrailTheme.colors.secondaryContainer,
+    headerRow: @Composable () -> Unit = {},
+    secondaryRow: @Composable () -> Unit = {},
+    iconsRow: @Composable () -> Unit = {},
+) {
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .clip(RoundedCornerShape(12.dp))
+        .background(backgroundColor)
+        .semantics(mergeDescendants = true) {}
+        .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        headerRow()
+        secondaryRow()
+        iconsRow()
+    }
+}
+
+@ComponentPreviews
+@Composable
+private fun BasicJourneyCardPreview() {
+    KrailTheme {
+        BasicJourneyCard(modifier = Modifier,
+            headerRow = { Text(text = "Header") },
+            secondaryRow = { Text(text = "Secondary") },
+            iconsRow = { Text(text = "Icons") }
+        )
+    }
+}
