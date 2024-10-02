67427a3b2fee5044f6cf1383dd7a6ab035f5a031
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-02 19:48:49 +1000

    UI: Update BasicJourneyCard to be more dynamic to accept UI changes

--- core/design-system/src/main/kotlin/xyz/ksharma/krail/design/system/components/BasicJourneyCard.kt
+++ core/design-system/src/main/kotlin/xyz/ksharma/krail/design/system/components/BasicJourneyCard.kt
@@ -1,16 +1,12 @@
 package xyz.ksharma.krail.design.system.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -23,9 +19,7 @@ import xyz.ksharma.krail.design.system.theme.KrailTheme
 internal fun BasicJourneyCard(
     modifier: Modifier = Modifier,
     backgroundColor: Color = KrailTheme.colors.secondaryContainer,
-    headerRow: @Composable RowScope.() -> Unit = {},
-    secondaryRow: @Composable RowScope.() -> Unit = {},
-    iconsRow: @Composable RowScope.() -> Unit = {},
+    content: @Composable ColumnScope.() -> Unit,
 ) {
     Column(modifier = modifier
         .fillMaxWidth()
@@ -34,20 +28,7 @@ internal fun BasicJourneyCard(
         .semantics(mergeDescendants = true) {}
         .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            headerRow()
-        }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            secondaryRow()
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 4.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            iconsRow()
-        }
+        content()
     }
 }
 
@@ -56,9 +37,9 @@ internal fun BasicJourneyCard(
 private fun BasicJourneyCardPreview() {
     KrailTheme {
         BasicJourneyCard(
-            headerRow = { Text(text = "Header") },
-            secondaryRow = { Text(text = "Secondary") },
-            iconsRow = { Text(text = "Icons") }
+            content = {
+                Text("content")
+            },
         )
     }
 }
