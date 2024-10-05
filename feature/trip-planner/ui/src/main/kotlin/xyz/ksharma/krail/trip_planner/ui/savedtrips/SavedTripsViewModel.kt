9b0004730225855511f54b88c265bc21d6965db9
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-05 15:24:01 +1000

    ViewModels (#128)

---
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/savedtrips/SavedTripsViewModel.kt
@@ -0,0 +1,10 @@
+package xyz.ksharma.krail.trip_planner.ui.savedtrips
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SavedTripsViewModel @Inject constructor() : ViewModel() {
+
+
+}
