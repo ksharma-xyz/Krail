a4711bf61048f30dd9003c55f61a5e24d68d0267
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-05 19:45:21 +1000

    UI: Ad UI for SavedTripsScreen (#133)

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/savedtrips/SavedTripsScreen.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/savedtrips/SavedTripsScreen.kt
@@ -7,9 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import xyz.ksharma.krail.design.system.components.Text
 import xyz.ksharma.krail.design.system.theme.KrailTheme
+import xyz.ksharma.krail.trip_planner.ui.R
 import xyz.ksharma.krail.trip_planner.ui.state.savedtrip.SavedTripUiEvent
 import xyz.ksharma.krail.trip_planner.ui.state.savedtrip.SavedTripsState
 
@@ -22,25 +26,28 @@ fun SavedTripsScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(color = KrailTheme.colors.secondaryContainer)
+            .background(color = KrailTheme.colors.background)
             .padding(start = 16.dp, end = 16.dp)
     ) {
+        // TODO -  refactor to a ScreenTitle component
         Text(
-            "SavedTripsScreen",
-            style = KrailTheme.typography.bodyLarge,
+            text = stringResource(R.string.saved_trips_screen_title),
+            style = KrailTheme.typography.headlineLarge,
             modifier = Modifier.padding(top = 16.dp)
         )
 
-        Text(
-            "state: ${savedTripsState.trip}",
-            style = KrailTheme.typography.bodySmall,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-
-        Text(
-            "Load State",
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .clickable { onEvent(SavedTripUiEvent.LoadSavedTrips) })
+        
     }
 }
+
+// region Previews
+
+@PreviewLightDark
+@Composable
+private fun SavedTripsScreenPreview() {
+    KrailTheme {
+        SavedTripsScreen(savedTripsState = SavedTripsState())
+    }
+}
+
+// endregion
