8f77e5953100be044e1b825ecc1bfd6f84b7bde5
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-05 19:39:51 +1000

    UI: Add SavedTripsScreenContent

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/components/SearchStopRow.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/components/SearchStopRow.kt
@@ -1,5 +1,6 @@
 package xyz.ksharma.krail.trip_planner.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,8 +15,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import xyz.ksharma.krail.design.system.LocalOnContentColor
 import xyz.ksharma.krail.design.system.components.RoundIconButton
 import xyz.ksharma.krail.design.system.components.TextField
 import xyz.ksharma.krail.design.system.preview.ComponentPreviews
@@ -29,7 +34,7 @@ fun SearchStopRow(modifier: Modifier = Modifier) {
             .fillMaxWidth()
             .background(
                 color = KrailTheme.colors.primary,
-                shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+                shape = RoundedCornerShape(topStart = 36.dp, topEnd = 36.dp)
             )
             .padding(vertical = 24.dp, horizontal = 16.dp),
         horizontalArrangement = Arrangement.End,
@@ -48,16 +53,30 @@ fun SearchStopRow(modifier: Modifier = Modifier) {
                 .padding(start = 16.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp) // TODO - token "SearchFieldSpacing"
         ) {
-            RoundIconButton {
-            }
+            RoundIconButton(
+                content = {
+                    Image(
+                        imageVector = ImageVector.vectorResource(TripPlannerUiR.drawable.ic_search),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(LocalOnContentColor.current),
+                    )
+                },
+                onClick = {},
+            )
 
-
-            RoundIconButton {
-            }
+            RoundIconButton(
+                content = {
+                    Image(
+                        imageVector = ImageVector.vectorResource(TripPlannerUiR.drawable.ic_search),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(LocalOnContentColor.current),
+                    )
+                },
+                onClick = {},
+            )
         }
     }
 }
-
 
 // region Previews
 
