9d1ba33c60cc764433c2f1ed9e3f4e9d3bf54b8c
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-05 16:24:30 +1000

    Add Fake SplashScreen and click events (#132)

--- app/src/main/kotlin/xyz/ksharma/krail/navigation/KrailNavHost.kt
+++ app/src/main/kotlin/xyz/ksharma/krail/navigation/KrailNavHost.kt
@@ -1,21 +1,37 @@
 package xyz.ksharma.krail.navigation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
 import kotlinx.serialization.Serializable
 import xyz.ksharma.krail.design.system.components.Text
 import xyz.ksharma.krail.design.system.theme.KrailTheme
 import xyz.ksharma.krail.trip_planner.ui.navigation.SavedTripsRoute
+import xyz.ksharma.krail.trip_planner.ui.navigation.navigateToSearchStopScreen
+import xyz.ksharma.krail.trip_planner.ui.navigation.navigateToTimeTableScreen
 import xyz.ksharma.krail.trip_planner.ui.navigation.tripPlannerDestinations
 
+/**
+ * TODO - I don't like [NavHost] defined in app module, I would love to refactor it to :core:navigation module
+ *    but that results in a cyclic dependency. Feature module needs to depend on :core:navigation for navigation logic and
+ *    then core:navigation needs to depend on feature module for the destinations / nested navigation graphs.
+ *    This results in putting all nav logic in the app module, which will have negative impacts on the build time.
+ *    Why is navigation so hard in Compose?
+ *
+ *   Navigation logic is currently taken from [NowInAndroid](https://github.com/android/nowinandroid] app,
+ *   so fine for now. But I will want to refactor it to something nicer e.g. using Circuit library
+ *   from Slack, but that would also mean refactoring to use MVP instead of MVVM.
+ */
 @Composable
 fun KrailNavHost() {
 
@@ -23,27 +39,50 @@ fun KrailNavHost() {
 
     NavHost(
         navController = navController,
-        startDestination = KrailNavRoute1,
+        startDestination = SplashScreen,
         modifier = Modifier
             .fillMaxSize()
             .safeContentPadding()
     ) {
-        tripPlannerDestinations()
+        tripPlannerDestinations(
+            onSearchStopClick = navController::navigateToSearchStopScreen,
+            onLoadTimeTableClick = navController::navigateToTimeTableScreen,
+        )
 
-        composable<KrailNavRoute1> {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = KrailTheme.colors.background)
-            ) {
-                // Add your composable here
-                Text(text = "Click", modifier = Modifier.clickable {
-                    navController.navigate(SavedTripsRoute)
-                })
+        composable<SplashScreen> {
+            SplashScreen {
+                navController.navigate(
+                    route = SavedTripsRoute,
+                    navOptions = NavOptions.Builder()
+                        .setLaunchSingleTop(true)
+                        .setPopUpTo<SplashScreen>(inclusive = true)
+                        .build()
+                )
             }
         }
     }
 }
 
+@Composable
+private fun SplashScreen(modifier: Modifier = Modifier, onSplashComplete: () -> Unit) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = KrailTheme.colors.background),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = "Krail", style = KrailTheme.typography.titleLarge)
+
+        // TODO
+        //  - Add Splash Screen Anim here
+        //  - Load Config
+
+        LaunchedEffect(key1 = Unit) {
+            delay(1000)
+            onSplashComplete()
+        }
+    }
+}
+
 @Serializable
-data object KrailNavRoute1
+data object SplashScreen
