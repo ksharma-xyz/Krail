ffa2b0f07e175a28433ff8e920629f6f1d4fdcaa
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-06 12:41:31 +1100

    UI: Add TextFieldButton (#138)

---
+++ core/design-system/src/main/kotlin/xyz/ksharma/krail/design/system/components/TextFieldButton.kt
@@ -0,0 +1,72 @@
+package xyz.ksharma.krail.design.system.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import xyz.ksharma.krail.design.system.LocalContentAlpha
+import xyz.ksharma.krail.design.system.LocalTextColor
+import xyz.ksharma.krail.design.system.LocalTextStyle
+import xyz.ksharma.krail.design.system.preview.ComponentPreviews
+import xyz.ksharma.krail.design.system.theme.KrailTheme
+import xyz.ksharma.krail.design.system.tokens.TextFieldTokens
+import xyz.ksharma.krail.design.system.tokens.TextFieldTokens.TextFieldHeight
+
+/**
+ * A button that looks like a text field.
+ *
+ * @param modifier The modifier to apply to this component.
+ * @param enabled Whether the button is enabled.
+ * @param content The content of the button.
+ *
+ * TODO - how to ensure modifiers for TextField and TextFieldButton are consistent?
+ */
+@Composable
+fun TextFieldButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    val contentAlpha = if (enabled) 1f else TextFieldTokens.DisabledLabelOpacity
+
+    CompositionLocalProvider(
+        LocalTextColor provides KrailTheme.colors.onSecondaryContainer,
+        LocalTextStyle provides KrailTheme.typography.titleLarge,
+        LocalContentAlpha provides contentAlpha,
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(TextFieldHeight)
+                .background(
+                    shape = RoundedCornerShape(TextFieldHeight.div(2)),
+                    color = KrailTheme.colors.secondaryContainer
+                )
+                .padding(horizontal = 16.dp, vertical = 4.dp),
+            contentAlignment = Alignment.CenterStart,
+        ) {
+            content()
+        }
+    }
+}
+
+// region Previews
+
+@ComponentPreviews
+@Composable
+private fun TextFieldButtonPreview() {
+    KrailTheme {
+        TextFieldButton {
+            Text(text = "Search")
+        }
+    }
+}
+
+// endregion
--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/components/SearchStopRow.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/components/SearchStopRow.kt
@@ -1,61 +1,40 @@
 package xyz.ksharma.krail.trip_planner.ui.components
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.relocation.BringIntoViewRequester
-import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import xyz.ksharma.krail.design.system.LocalOnContentColor
 import xyz.ksharma.krail.design.system.components.RoundIconButton
-import xyz.ksharma.krail.design.system.components.TextField
+import xyz.ksharma.krail.design.system.components.Text
+import xyz.ksharma.krail.design.system.components.TextFieldButton
 import xyz.ksharma.krail.design.system.preview.ComponentPreviews
 import xyz.ksharma.krail.design.system.theme.KrailTheme
 import xyz.ksharma.krail.trip_planner.ui.R as TripPlannerUiR
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SearchStopRow(modifier: Modifier = Modifier) {
-
-    val bringIntoViewRequester = remember { BringIntoViewRequester() }
-    val keyboardController = LocalSoftwareKeyboardController.current
-    var isTextFieldFocused by remember { mutableStateOf(false) }
-
-    LaunchedEffect(isTextFieldFocused) {
-        if (isTextFieldFocused) {
-            bringIntoViewRequester.bringIntoView()
-            keyboardController?.show()
-        }
-    }
-
     Row(
         modifier = modifier
             .fillMaxWidth()
             .background(
                 color = KrailTheme.colors.primary,
-                shape = RoundedCornerShape(topStart = 36.dp, topEnd = 36.dp)
+                shape = RoundedCornerShape(36.dp)
             )
-            .padding(vertical = 24.dp, horizontal = 16.dp)
-            .bringIntoViewRequester(bringIntoViewRequester),
+            .padding(vertical = 24.dp, horizontal = 16.dp),
         horizontalArrangement = Arrangement.End,
     ) {
 
@@ -63,21 +42,12 @@ fun SearchStopRow(modifier: Modifier = Modifier) {
             modifier = Modifier.weight(0.8f),
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-            TextField(
-                initialText = stringResource(TripPlannerUiR.string.from_text_field_placeholder),
-                modifier = Modifier
-                    .onFocusChanged {
-                        isTextFieldFocused = it.isFocused
-                    },
-            )
-
-            TextField(
-                initialText = stringResource(TripPlannerUiR.string.to_text_field_placeholder),
-                modifier = Modifier
-                    .onFocusChanged {
-                        isTextFieldFocused = it.isFocused
-                    },
-            )
+            TextFieldButton {
+                Text(text = stringResource(TripPlannerUiR.string.from_text_field_placeholder))
+            }
+            TextFieldButton{
+                Text(text = stringResource(TripPlannerUiR.string.to_text_field_placeholder))
+            }
         }
 
         Column(
