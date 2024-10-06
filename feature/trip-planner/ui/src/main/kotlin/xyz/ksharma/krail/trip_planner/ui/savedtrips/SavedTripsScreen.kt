a8472acc1943b258f50cb0d12c00f36f60a0d5ea
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-06 14:09:22 +1100

    UI: Add Title Bar (#140)

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/savedtrips/SavedTripsScreen.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/savedtrips/SavedTripsScreen.kt
@@ -4,9 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import xyz.ksharma.krail.design.system.components.Text
+import xyz.ksharma.krail.design.system.components.TitleBar
 import xyz.ksharma.krail.design.system.theme.KrailTheme
 import xyz.ksharma.krail.trip_planner.ui.R
 import xyz.ksharma.krail.trip_planner.ui.components.SearchStopRow
@@ -38,15 +35,7 @@ fun SavedTripsScreen(
 
         LazyColumn(modifier = Modifier, contentPadding = PaddingValues(bottom = 300.dp)) {
             item {
-                Text(
-                    text = stringResource(R.string.saved_trips_screen_title),
-                    style = KrailTheme.typography.headlineLarge,
-                    color = KrailTheme.colors.onSecondaryContainer,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                        .systemBarsPadding()
-                )
+                TitleBar(title = stringResource(R.string.saved_trips_screen_title))
             }
         }
 
