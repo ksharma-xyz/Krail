e2704a25719c49e8f55d366294494bfbaac48842
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-12 23:38:48 +1100

    Delete GTFS Model class

--- core/model/src/main/kotlin/xyz/ksharma/krail/model/DemoData.kt
+++
@@ -1,5 +0,0 @@
-package xyz.ksharma.krail.model
-
-data class DemoData(val list: List<Item>)
-
-data class Item(val value: String, val id: String)
--- feature/sydney-trains/database/real/src/main/kotlin/xyz/ksharma/krail/sydney/trains/database/real/parser/ParserExt.kt
+++
@@ -1,3 +0,0 @@
-package xyz.ksharma.krail.sydney.trains.database.real.parser
-
-internal fun List<String>.trimQuotes(): List<String> = this.map { it.trim('\"') }
