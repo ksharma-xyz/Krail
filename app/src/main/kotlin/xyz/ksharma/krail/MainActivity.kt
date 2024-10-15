d69dfba1301137873873849b5446002e9921fa02
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-15 18:44:36 +1100

    Fix white screen issue https://issuetracker.google.com/issues/300279937 https://issuetracker.google.com/issues/296001700

--- app/src/main/kotlin/xyz/ksharma/krail/MainActivity.kt
+++ app/src/main/kotlin/xyz/ksharma/krail/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.ksharma.krail.design.system.theme.KrailTheme
 
@@ -12,8 +13,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Configures the system bars with a transparent background.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
 
         setContent {
