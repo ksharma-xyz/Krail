4856a1825865741d99ffc667e118bb33a1fc2276
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-15 19:59:51 +1100

    Add animation

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/timetable/TimeTableScreen.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/timetable/TimeTableScreen.kt
@@ -1,5 +1,12 @@
 package xyz.ksharma.krail.trip_planner.ui.timetable
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,6 +16,11 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -53,16 +65,29 @@ fun TimeTableScreen(
             }
         } else if (timeTableState.journeyList.isNotEmpty()) {
             items(timeTableState.journeyList) { journey ->
-                if (expandedJourneyId == journey.journeyId) {
+                var isFirstTime by rememberSaveable { mutableStateOf(true) }
+
+                AnimatedVisibility(
+                    visible = expandedJourneyId == journey.journeyId,
+                    enter = scaleIn(initialScale = 0.3f) + fadeIn(),
+                    exit = fadeOut() + shrinkOut(),
+                ) {
                     JourneyDetailCard(
                         header = journey.timeText + journey.platformText?.let { " on ${journey.platformText}" } + " (${journey.travelTime})",
                         journeyLegList = journey.legs,
                         onClick = {
                             onEvent(TimeTableUiEvent.JourneyCardClicked(journey.journeyId))
                         },
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                            .animateContentSize(),
                     )
-                } else {
+                }
+                AnimatedVisibility(
+                    visible = expandedJourneyId != journey.journeyId,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                ) {
                     JourneyCardItem(
                         departureTimeText = journey.timeText,
                         departureLocationText = journey.platformText?.let { "on ${journey.platformText}" },
