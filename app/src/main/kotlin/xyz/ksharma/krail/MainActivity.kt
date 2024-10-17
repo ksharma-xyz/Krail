9e61b21e1cdf5cb254856832ada828922d7c12db
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-17 21:11:29 +1100

    Fix navigation bars padding and colors (#198)

--- app/src/main/kotlin/xyz/ksharma/krail/MainActivity.kt
+++ app/src/main/kotlin/xyz/ksharma/krail/MainActivity.kt
@@ -1,10 +1,10 @@
 package xyz.ksharma.krail
 
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.ksharma.krail.design.system.theme.KrailTheme
 
@@ -13,8 +13,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
+        // Required because https://issuetracker.google.com/issues/298296168
+        // Edge-to-edge does not draw behind 3-button nav
+        window.setFlags(FLAG_LAYOUT_NO_LIMITS, FLAG_LAYOUT_NO_LIMITS)
 
         setContent {
             KrailTheme {
