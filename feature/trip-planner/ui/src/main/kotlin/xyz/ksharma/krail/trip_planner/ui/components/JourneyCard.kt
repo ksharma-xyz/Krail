fb7ba8c8529cf5ce69a01d95690484afb13935a2
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-19 00:14:24 +1100

    UI: Create JourneyCard - updated UI (#202)

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/components/JourneyCard.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip_planner/ui/components/JourneyCard.kt
@@ -1,27 +1,235 @@
 package xyz.ksharma.krail.trip_planner.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import xyz.ksharma.krail.design.system.LocalTextColor
 import xyz.ksharma.krail.design.system.LocalTextStyle
 import xyz.ksharma.krail.design.system.components.BasicJourneyCard
 import xyz.ksharma.krail.design.system.components.SeparatorIcon
 import xyz.ksharma.krail.design.system.components.Text
-import xyz.ksharma.krail.design.system.preview.ComponentPreviews
 import xyz.ksharma.krail.design.system.theme.KrailTheme
+import xyz.ksharma.krail.design.system.toAdaptiveDecorativeIconSize
+import xyz.ksharma.krail.design.system.toAdaptiveSize
+import xyz.ksharma.krail.trip_planner.ui.R
+
+/**
+ * A card that displays information about a journey.
+ * @param timeToDeparture The time until the journey departs.
+ * @param originTime The time the journey starts.
+ * @param destinationTime The time the journey ends.
+ * @param totalTravelTime The total time the journey takes.
+ * @param platformNumber The platform or stand number, the journey departs from.
+ * @param isWheelchairAccessible Whether the journey is wheelchair accessible.
+ * @param transportModeIconRow A row of icons representing the transport modes used in the journey.
+ * @param onClick The action to perform when the card is clicked.
+ * @param modifier The modifier to apply to the card.
+ */
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun JourneyCard(
+    timeToDeparture: String,
+    originTime: String,
+    destinationTime: String,
+    totalTravelTime: String,
+    platformNumber: Char,
+    isWheelchairAccessible: Boolean,
+    transportModeIconRow: @Composable RowScope.() -> Unit,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .background(color = KrailTheme.colors.surface)
+            .clickable(role = Role.Button, onClick = onClick)
+            .padding(vertical = 8.dp, horizontal = 12.dp),
+    ) {
+        FlowRow(
+            horizontalArrangement = Arrangement.Start,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = timeToDeparture, style = KrailTheme.typography.titleMedium,
+                color = "#127766".hexToComposeColor(),
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Row(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+            ) {
+                transportModeIconRow()
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .size(28.dp.toAdaptiveDecorativeIconSize())
+                    .border(
+                        width = 3.dp,
+                        color = "#127766".hexToComposeColor(),
+                        shape = CircleShape
+                    ),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = platformNumber.toString(),
+                    textAlign = TextAlign.Center,
+                    style = KrailTheme.typography.labelLarge,
+                )
+            }
+        }
+
+        Text(
+            text = originTime,
+            style = KrailTheme.typography.titleMedium,
+            color = KrailTheme.colors.onSurface
+        )
+
+        FlowRow(
+            horizontalArrangement = Arrangement.Start,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally),
+        ) {
+            Text(
+                text = destinationTime,
+                style = KrailTheme.typography.titleMedium,
+                color = KrailTheme.colors.onSurface,
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.align(Alignment.CenterVertically),
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_clock),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(color = KrailTheme.colors.onBackground),
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .align(Alignment.CenterVertically)
+                        .size(14.dp.toAdaptiveSize()),
+                )
+                Text(
+                    text = totalTravelTime, style = KrailTheme.typography.bodyMedium,
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            if (isWheelchairAccessible) {
+                Image(
+                    painter = painterResource(R.drawable.ic_a11y),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(color = KrailTheme.colors.onBackground),
+                    modifier = Modifier
+                        .size(14.dp.toAdaptiveSize())
+                        .align(Alignment.CenterVertically),
+                )
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Preview(fontScale = 2f)
+@Composable
+private fun PreviewJourneyCard() {
+    KrailTheme {
+        JourneyCard(
+            timeToDeparture = "in 5 mins",
+            originTime = "8:25am",
+            destinationTime = "8:40am",
+            totalTravelTime = "15 mins",
+            platformNumber = '1',
+            isWheelchairAccessible = true,
+            transportModeIconRow = {
+                TransportModeIcon(
+                    letter = 'T',
+                    backgroundColor = "#F59A24".hexToComposeColor(),
+                )
+                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+                TransportModeIcon(
+                    letter = 'B',
+                    backgroundColor = "#00B5EF".hexToComposeColor(),
+                )
+            },
+            onClick = {},
+        )
+    }
+}
+
+@Preview
+@Preview(fontScale = 2f)
+@Composable
+private fun PreviewJourneyCardLongData() {
+    KrailTheme {
+        JourneyCard(
+            timeToDeparture = "in 1h 5mins",
+            originTime = "12:25am",
+            destinationTime = "12:40am",
+            totalTravelTime = "45h 15minutes",
+            platformNumber = 'A',
+            isWheelchairAccessible = true,
+            transportModeIconRow = {
+                TransportModeIcon(
+                    letter = 'T',
+                    backgroundColor = "#F59A24".hexToComposeColor(),
+                )
+                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+                TransportModeIcon(
+                    letter = 'B',
+                    backgroundColor = "#00B5EF".hexToComposeColor(),
+                )
+                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+                TransportModeIcon(
+                    letter = 'T',
+                    backgroundColor = "#F59A24".hexToComposeColor(),
+                )
+                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+                TransportModeIcon(
+                    letter = 'T',
+                    backgroundColor = "#F59A24".hexToComposeColor(),
+                )
+                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+                TransportModeIcon(
+                    letter = 'B',
+                    backgroundColor = "#00B5EF".hexToComposeColor(),
+                )
+            },
+            onClick = {},
+        )
+    }
+}
+
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
@@ -30,7 +238,7 @@ fun JourneyCard(
     departureLocationText: String? = null,
     originAndDestinationTimeText: String,
     durationText: String,
-    backgroundColor: Color = KrailTheme.colors.tertiaryContainer.copy(alpha = 0.8f),
+    backgroundColor: Color = KrailTheme.colors.surface,
     transportModeIconRow: @Composable RowScope.() -> Unit,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -110,79 +318,80 @@ fun JourneyCard(
 }
 
 // region Previews
-
+/**
 @Preview
 @Preview(fontScale = 2f)
 @Composable
 private fun JourneyCardTrainLongTextPreview() {
-    KrailTheme {
-        JourneyCard(
-            departureTimeText = "in 2h 3mins",
-            departureLocationText = "on Parramatta Station, Stand A",
-            originAndDestinationTimeText = "8:25am - 8:40am",
-            durationText = "23 mins",
-            transportModeIconRow = {
-                TransportModeInfo(
-                    letter = 'T',
-                    backgroundColor = "#005aa3".hexToComposeColor(),
-                    badgeColor = "#005aa3".hexToComposeColor(),
-                    badgeText = "T4",
-                )
-                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
-                TransportModeInfo(
-                    letter = 'T',
-                    backgroundColor = "#005aa3".hexToComposeColor(),
-                    badgeText = "T4",
-                )
-                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
-                TransportModeInfo(
-                    letter = 'T',
-                    backgroundColor = "#005aa3".hexToComposeColor(),
-                    badgeText = "T4",
-                )
-                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
-                TransportModeInfo(
-                    letter = 'T',
-                    backgroundColor = "#005aa3".hexToComposeColor(),
-                    badgeText = "T4",
-                )
-                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
-                TransportModeInfo(
-                    letter = 'T',
-                    backgroundColor = "#005aa3".hexToComposeColor(),
-                    badgeText = "T4",
-                )
-            },
-            onClick = {}
-        )
-    }
+KrailTheme {
+JourneyCard(
+departureTimeText = "in 2h 3mins",
+departureLocationText = "on Parramatta Station, Stand A",
+originAndDestinationTimeText = "8:25am - 8:40am",
+durationText = "23 mins",
+transportModeIconRow = {
+TransportModeInfo(
+letter = 'T',
+backgroundColor = "#005aa3".hexToComposeColor(),
+badgeColor = "#005aa3".hexToComposeColor(),
+badgeText = "T4",
+)
+SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+TransportModeInfo(
+letter = 'T',
+backgroundColor = "#005aa3".hexToComposeColor(),
+badgeText = "T4",
+)
+SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+TransportModeInfo(
+letter = 'T',
+backgroundColor = "#005aa3".hexToComposeColor(),
+badgeText = "T4",
+)
+SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+TransportModeInfo(
+letter = 'T',
+backgroundColor = "#005aa3".hexToComposeColor(),
+badgeText = "T4",
+)
+SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+TransportModeInfo(
+letter = 'T',
+backgroundColor = "#005aa3".hexToComposeColor(),
+badgeText = "T4",
+)
+},
+onClick = {}
+)
+}
 }
 
 @ComponentPreviews
 @Composable
 private fun JourneyCardMultipleModesPreview() {
-    KrailTheme {
-        JourneyCard(
-            departureTimeText = "in 5 mins",
-            departureLocationText = "on Platform 1",
-            originAndDestinationTimeText = "8:25am - 8:40am",
-            durationText = "15 mins",
-            transportModeIconRow = {
-                TransportModeInfo(
-                    letter = 'T',
-                    backgroundColor = "#005aa3".hexToComposeColor(),
-                    badgeText = "T4",
-                )
-                SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
-                TransportModeInfo(
-                    letter = 'B',
-                    backgroundColor = "#00B5EF".hexToComposeColor(),
-                    badgeText = "700",
-                )
-            },
-            onClick = {},
-        )
-    }
+KrailTheme {
+JourneyCard(
+departureTimeText = "in 5 mins",
+departureLocationText = "on Platform 1",
+originAndDestinationTimeText = "8:25am - 8:40am",
+durationText = "15 mins",
+transportModeIconRow = {
+TransportModeInfo(
+letter = 'T',
+backgroundColor = "#005aa3".hexToComposeColor(),
+badgeText = "T4",
+)
+SeparatorIcon(modifier = Modifier.align(Alignment.CenterVertically))
+TransportModeInfo(
+letter = 'B',
+backgroundColor = "#00B5EF".hexToComposeColor(),
+badgeText = "700",
+)
+},
+onClick = {},
+)
+}
 }
 
 // endregion
+ */
