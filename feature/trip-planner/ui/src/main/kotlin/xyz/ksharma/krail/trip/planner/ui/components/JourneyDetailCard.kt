467bc0c9c37d1857ed7563810fca73267428d218
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-23 22:30:01 +1100

    UI: Integrate JourneyCardState in JourneyCard composable and add different types of content

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip/planner/ui/components/JourneyDetailCard.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip/planner/ui/components/JourneyDetailCard.kt
@@ -237,7 +237,7 @@ fun JourneyDetailCard(
     }
 }
 
-private fun TransportMode.buildPlatformText(platformNumber: Char): String? {
+fun TransportMode.buildPlatformText(platformNumber: Char): String? {
     return when (this) {
         is TransportMode.Train, is TransportMode.Metro -> "Platform $platformNumber"
         is TransportMode.Bus -> "Stand $platformNumber"
@@ -464,22 +464,3 @@ fun JourneyDetailCard(
         }
     }
 }*/
-
-enum class JourneyCardState {
-    /**
-     * Default state of the card. as displayed in the list.
-     */
-    DEFAULT,
-
-    /**
-     * Card displaying information about different legs of the journey.
-     * The number of stops and duration of each leg.
-     */
-    COLLAPSED,
-
-    /**
-     * Card displaying the full journey details of a certain leg.
-     * It will display all the stops in the leg, with platform and timing information.
-     */
-    EXPANDED,
-}
