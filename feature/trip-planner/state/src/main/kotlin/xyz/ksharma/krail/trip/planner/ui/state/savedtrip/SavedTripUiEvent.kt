7632c4cd6ae6b0e2efc42ed7d4743676a67c6ad5
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-31 00:35:56 +1100

    UI: Remove SavedTripClicked event and refactor SavedTripsViewModel (#267)

--- feature/trip-planner/state/src/main/kotlin/xyz/ksharma/krail/trip/planner/ui/state/savedtrip/SavedTripUiEvent.kt
+++ feature/trip-planner/state/src/main/kotlin/xyz/ksharma/krail/trip/planner/ui/state/savedtrip/SavedTripUiEvent.kt
@@ -4,6 +4,5 @@ import xyz.ksharma.krail.trip.planner.ui.state.timetable.Trip
 
 sealed interface SavedTripUiEvent {
     data object LoadSavedTrips : SavedTripUiEvent
-    data class SavedTripClicked(val trip: Trip) : SavedTripUiEvent
     data class DeleteSavedTrip(val trip: Trip) : SavedTripUiEvent
 }
