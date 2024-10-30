9afb97bacbde1d62d12646e111ff5e72678ddb3e
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-10-30 16:56:21 +1100

    Calculate color code using Transport Mode

--- feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip/planner/ui/components/ColorExt.kt
+++ feature/trip-planner/ui/src/main/kotlin/xyz/ksharma/krail/trip/planner/ui/components/ColorExt.kt
@@ -1,6 +1,9 @@
 package xyz.ksharma.krail.trip.planner.ui.components
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import xyz.ksharma.krail.trip.planner.ui.state.TransportMode
 
 /**
  * Converts a hexadecimal color string to a Compose Color object.
@@ -33,4 +36,21 @@ private fun String.isValidHexColorCode(): Boolean {
     // Regular expression to match #RRGGBB or #RRGGBBAA hex color codes
     val hexColorRegex = Regex("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})\$")
     return hexColorRegex.matches(this)
+}
+
+/**
+ * Will return a color based on the transport mode and the current theme (light / dark).
+ *
+ * @throws IllegalArgumentException if the provided string is not a valid
+ * hexadecimal color code.
+ *
+ * @return A Compose Color object representing the provided hex color code.
+ */
+@Composable
+internal fun transportModeBackgroundColor(transportMode: TransportMode): Color {
+    return if (isSystemInDarkTheme()) {
+        transportMode.colorCode.hexToComposeColor().copy(alpha = 0.45f)
+    } else {
+        transportMode.colorCode.hexToComposeColor().copy(alpha = 0.15f)
+    }
 }
