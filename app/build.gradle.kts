920f48e27f82678b6ff9f57950f31b372d531581
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-11-01 14:20:25 +1100

    Add firebase crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,7 @@ dependencies {
     // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
 
     // Test
     androidTestImplementation(libs.test.androidxTestExtJunit)
