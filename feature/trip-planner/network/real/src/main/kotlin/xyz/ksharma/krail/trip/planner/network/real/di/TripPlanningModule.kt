c0e1520d925c51cb425cadfe048d14799b472a9f
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-11-03 22:13:12 +1100

    Add API rate limiting for trip planner requests (#294)

--- feature/trip-planner/network/real/src/main/kotlin/xyz/ksharma/krail/trip/planner/network/real/di/TripPlanningModule.kt
+++ feature/trip-planner/network/real/src/main/kotlin/xyz/ksharma/krail/trip/planner/network/real/di/TripPlanningModule.kt
@@ -4,7 +4,9 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import xyz.ksharma.krail.trip.planner.network.api.RateLimiter
 import xyz.ksharma.krail.trip.planner.network.api.repository.TripPlanningRepository
+import xyz.ksharma.krail.trip.planner.network.real.ratelimit.APIRateLimiter
 import xyz.ksharma.krail.trip.planner.network.real.repository.RealTripPlanningRepository
 import javax.inject.Singleton
 
@@ -15,4 +17,8 @@ abstract class TripPlanningModule {
     @Binds
     @Singleton
     abstract fun bindTripPlanningRepository(impl: RealTripPlanningRepository): TripPlanningRepository
+
+    @Binds
+    // This clas should not be Singleton. It should be created per use-case.
+    abstract fun bindRateLimiter(impl: APIRateLimiter): RateLimiter
 }
