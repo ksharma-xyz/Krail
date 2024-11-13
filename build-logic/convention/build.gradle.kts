4526da5c382d6212f166b8b266b7789ad3b3d4a4
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-11-13 20:20:41 +1100

    Build: Update Kotlin JVM target and remove unused module (#341)

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -14,8 +15,8 @@ java {
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "$javaVersion"
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
