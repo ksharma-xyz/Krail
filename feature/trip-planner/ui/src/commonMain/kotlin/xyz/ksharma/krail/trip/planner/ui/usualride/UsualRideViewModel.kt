3846d637ed2f91a6b0216590fc7789efe184ffee
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-11-19 23:57:45 +1100

    update UsualRideViewModel

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/usualride/UsualRideViewModel.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/usualride/UsualRideViewModel.kt
@@ -2,24 +2,20 @@ package xyz.ksharma.krail.trip.planner.ui.usualride
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import xyz.ksharma.krail.di.AppDispatchers
-import xyz.ksharma.krail.di.Dispatcher
+import xyz.ksharma.krail.sandook.RealSandook
 import xyz.ksharma.krail.sandook.Sandook
-import xyz.ksharma.krail.sandook.di.SandookFactory
 import xyz.ksharma.krail.trip.planner.ui.state.TransportMode
 import xyz.ksharma.krail.trip.planner.ui.state.usualride.UsualRideEvent
 import xyz.ksharma.krail.trip.planner.ui.state.usualride.UsualRideState
 
-class UsualRideViewModel(
-    sandookFactory: SandookFactory,
-    @Dispatcher(AppDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
-) : ViewModel() {
+class UsualRideViewModel : ViewModel() {
 
-    private val sandook: Sandook = sandookFactory.create(SandookFactory.SandookKey.THEME)
+    private val sandook: Sandook = RealSandook()
 
     private val _uiState: MutableStateFlow<UsualRideState> = MutableStateFlow(UsualRideState())
     val uiState: StateFlow<UsualRideState> = _uiState
@@ -31,7 +27,7 @@ class UsualRideViewModel(
     }
 
     private fun onTransportModeSelected(productClass: Int) {
-        viewModelScope.launch(ioDispatcher) {
+        viewModelScope.launch(Dispatchers.IO) {
             TransportMode.toTransportModeType(productClass)?.let { mode ->
                 //Timber.d("onTransportModeSelected: $mode")
                 sandook.putInt("selectedMode", mode.productClass)
