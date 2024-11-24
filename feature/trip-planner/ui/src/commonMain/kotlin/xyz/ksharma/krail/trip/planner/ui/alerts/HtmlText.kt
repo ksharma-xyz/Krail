7aa536fe3b130ced3169011b3dabb66eed5f1f5b
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-11-24 22:06:49 +1100

    update collapsible alert

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/alerts/HtmlText.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/alerts/HtmlText.kt
@@ -2,6 +2,13 @@ package xyz.ksharma.krail.trip.planner.ui.alerts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
-expect fun HtmlText(text: String, modifier: Modifier = Modifier, onClick: () -> Unit)
+expect fun HtmlText(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    color: Color,
+    urlColor: Color,
+)
--- feature/trip-planner/ui/src/iosMain/kotlin/HtmlText.kt
+++ feature/trip-planner/ui/src/iosMain/kotlin/HtmlText.kt
@@ -3,11 +3,18 @@ package xyz.ksharma.krail.trip.planner.ui.alerts
 import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import xyz.ksharma.krail.taj.components.Text
 
 @Composable
-actual fun HtmlText(text: String, modifier: Modifier, onClick: () -> Unit) {
-    Text(text = replaceHtmlTags(text), modifier = Modifier.clickable { onClick() })
+actual fun HtmlText(
+    text: String,
+    modifier: Modifier,
+    onClick: () -> Unit,
+    color: Color,
+    urlColor: Color,
+) {
+    Text(text = replaceHtmlTags(text), modifier = Modifier.clickable { onClick() }, color = color)
 }
 
 // TODO - A workaround for iOS until https://issuetracker.google.com/issues/139326648 is fixed.
