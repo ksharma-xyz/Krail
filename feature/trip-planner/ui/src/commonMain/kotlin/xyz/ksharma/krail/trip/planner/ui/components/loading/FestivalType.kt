f4bcc0d607673216fcf145967bd214280324e6a2
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-11-26 18:48:13 +1100

    Sanitise emojis

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/components/loading/FestivalType.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/components/loading/FestivalType.kt
@@ -1,15 +1,13 @@
 package xyz.ksharma.krail.trip.planner.ui.components.loading
 
 enum class FestivalType {
-    AUSTRALIA_DAY,
     CHRISTMAS,
+    BOXING_DAY,
+    NEW_YEAR_EVE,
     NEW_YEAR,
     ANZAC_DAY,
-    MOTHERS_DAY,
-    FATHERS_DAY,
     EASTER,
     VALENTINES_DAY,
     HALLOWEEN,
     CHINESE_NEW_YEAR,
-    DIWALI,
 }
