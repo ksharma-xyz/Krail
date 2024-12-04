e4f938c9c4707b6474455da19974ef150e33c677
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-04 22:26:03 +1100

    Clean up DateTimeSelectorViewModel

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/datetimeselector/DateTimeSelectorDestination.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/datetimeselector/DateTimeSelectorDestination.kt
@@ -4,16 +4,12 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import org.koin.compose.viewmodel.koinViewModel
 import xyz.ksharma.krail.trip.planner.ui.navigation.DateTimeSelectorRoute
 import xyz.ksharma.krail.trip.planner.ui.state.datetimeselector.DateTimeSelectionItem
 
 internal fun NavGraphBuilder.dateTimeSelectorDestination(navController: NavHostController) {
     composable<DateTimeSelectorRoute> { backStackEntry ->
-        val viewModel: DateTimeSelectorViewModel = koinViewModel<DateTimeSelectorViewModel>()
         val route: DateTimeSelectorRoute = backStackEntry.toRoute()
-
-        // TODO - If arguments have data, then display that data in the Screen else display current date time.
 
         DateTimeSelectorScreen(
             dateTimeSelection = route.dateTimeSelectionItemJson?.let{
