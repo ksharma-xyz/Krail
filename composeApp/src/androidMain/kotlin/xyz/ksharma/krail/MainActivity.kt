44b9fe59de618cc7d7489c57d08673d155d03460
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-09 22:32:55 +1100

    MainActivity: Remove unused code and comments (#445)

--- composeApp/src/androidMain/kotlin/xyz/ksharma/krail/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/xyz/ksharma/krail/MainActivity.kt
@@ -4,35 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import xyz.ksharma.krail.KrailApp
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
-/*
-        val applicationComponent = AndroidApplicationComponent.from(this)
-*/
-
         setContent {
             KrailApp()
         }
     }
-
-    /*
-        private val koinConfig1 = koinConfiguration {
-            androidContext(androidContext = this@MainActivity.applicationContext)
-            modules(androidDbModule)
-            includes(koinConfig)
-        }
-    */
 }
-
-/*
-private fun AndroidApplicationComponent.Companion.from(context: Context): AndroidApplicationComponent {
-    return (context.applicationContext as KrailApplication).component
-}
-*/
