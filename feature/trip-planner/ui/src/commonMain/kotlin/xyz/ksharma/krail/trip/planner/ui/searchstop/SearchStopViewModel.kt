ea25dec92ccbf93f07ccd76e602fa2ae4b50eac0
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-11 00:24:15 +1100

    Remove api logic, only keep local data for stops

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/searchstop/SearchStopViewModel.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/searchstop/SearchStopViewModel.kt
@@ -8,13 +8,11 @@ import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import xyz.ksharma.krail.trip.planner.network.api.service.TripPlanningService
-import xyz.ksharma.krail.trip.planner.ui.searchstop.StopResultMapper.toStopResults
 import xyz.ksharma.krail.trip.planner.ui.state.TransportMode
 import xyz.ksharma.krail.trip.planner.ui.state.searchstop.SearchStopState
 import xyz.ksharma.krail.trip.planner.ui.state.searchstop.SearchStopUiEvent
@@ -39,44 +37,28 @@ class SearchStopViewModel(private val tripPlanningService: TripPlanningService) 
         searchJob = viewModelScope.launch(Dispatchers.IO) {
             val localResults = processLocalStopResults(query)
             updateUiState { displayData(localResults) }
-
-            println("NEW Stop Search")
-            runCatching {
-                val response = tripPlanningService.stopFinder(stopSearchQuery = query)
-                println("response VM: $response")
-
-                val networkResults = response.toStopResults()
-                networkResults.forEach {
-                    println("Network: stopId: ${it.stopId}, stopName: ${it.stopName}, transportModeType: ${it.transportModeType}")
-                }
-                val mergedResults = mergeResults(localResults, networkResults)
-                mergedResults.forEach {
-                    println("Merged: stopId: ${it.stopId}, stopName: ${it.stopName}, transportModeType: ${it.transportModeType}")
-                }
-
-                updateUiState { displayData(mergedResults) }
-            }.getOrElse {
-                delay(1500) // buffer for API response before displaying error.
-                updateUiState { displayError() }
-            }
         }
     }
 
     private fun processLocalStopResults(query: String): List<SearchStopState.StopResult> {
         val resultMap = LinkedHashMap<String, SearchStopState.StopResult>()
 
-        val normalizedQuery = query.replace(" ", "").lowercase()
-        fun normalizeStopName(name: String) = name.replace(" ", "").lowercase()
+        val normalizedQuery = query.replace(" ", "")
+        fun normalizeStopName(name: String) = name.replace(" ", "")
 
         // Filter metroStops, trainStops, ferryStops, and lightRailStops based on the normalized query
-        val matchingMetroStops =
-            metroStops.filter { normalizeStopName(it.value).contains(normalizedQuery) }
-        val matchingTrainStops =
-            trainStops.filter { normalizeStopName(it.value).contains(normalizedQuery) }
-        val matchingFerryStops =
-            ferryStops.filter { normalizeStopName(it.value).contains(normalizedQuery) }
-        val matchingLightRailStops =
-            lightRailStops.filter { normalizeStopName(it.value).contains(normalizedQuery) }
+        val matchingMetroStops = metroStops.filter {
+            normalizeStopName(it.value).contains(normalizedQuery, ignoreCase = true)
+        }
+        val matchingTrainStops = trainStops.filter {
+            normalizeStopName(it.value).contains(normalizedQuery, ignoreCase = true)
+        }
+        val matchingFerryStops = ferryStops.filter {
+            normalizeStopName(it.value).contains(normalizedQuery, ignoreCase = true)
+        }
+        val matchingLightRailStops = lightRailStops.filter {
+            normalizeStopName(it.value).contains(normalizedQuery, ignoreCase = true)
+        }
 
         // Create StopResult objects for matching metro stops
         matchingMetroStops.forEach { (id, name) ->
@@ -142,31 +124,13 @@ class SearchStopViewModel(private val tripPlanningService: TripPlanningService) 
         }
 
         // Sort results: exact matches first, then starts with query, then contains query
-        return resultMap.values.sortedWith(compareBy(
-            { !it.stopName.equals(query, ignoreCase = true) },
-            { !it.stopName.startsWith(query, ignoreCase = true) },
-            { it.stopName }
-        ))
-    }
-
-    private fun mergeResults(
-        localResults: List<SearchStopState.StopResult>,
-        networkResults: List<SearchStopState.StopResult>,
-    ): List<SearchStopState.StopResult> {
-        val resultMap = LinkedHashMap<String, SearchStopState.StopResult>()
-
-        (localResults + networkResults).forEach { result ->
-            val existingResult = resultMap[result.stopId]
-            if (existingResult != null) {
-                val combinedModes =
-                    (existingResult.transportModeType + result.transportModeType).toPersistentList()
-                resultMap[result.stopId] = existingResult.copy(transportModeType = combinedModes)
-            } else {
-                resultMap[result.stopId] = result
-            }
-        }
-
-        return resultMap.values.toList()
+        return resultMap.values.sortedWith(
+            compareBy(
+                { !it.stopName.equals(query, ignoreCase = true) },
+                { !it.stopName.startsWith(query, ignoreCase = true) },
+                { it.stopName.contains(query, ignoreCase = true) },
+            )
+        )
     }
 
     private fun SearchStopState.displayData(stopsResult: List<SearchStopState.StopResult>) = copy(
