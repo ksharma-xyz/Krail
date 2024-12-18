f1c2da522e7cfd06c6fca1692133f5d36961e2c8
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-18 22:37:58 +1100

    Bump app version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,7 +5,7 @@ android {
 
     defaultConfig {
         applicationId = "xyz.ksharma.krail"
-        versionCode = 29
+        versionCode = 30
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
