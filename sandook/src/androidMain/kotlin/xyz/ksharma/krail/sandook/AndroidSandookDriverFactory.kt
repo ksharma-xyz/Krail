e6962a8e517a1f8419efa6b5b1cb150e3844f1d6
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-25 14:05:59 +1100

    Add migration for creating new service alert table

--- sandook/src/androidMain/kotlin/xyz/ksharma/krail/sandook/AndroidSandookDriverFactory.kt
+++ sandook/src/androidMain/kotlin/xyz/ksharma/krail/sandook/AndroidSandookDriverFactory.kt
@@ -1,10 +1,7 @@
 package xyz.ksharma.krail.sandook
 
 import android.content.Context
-import androidx.sqlite.db.SupportSQLiteDatabase
-import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 
 class AndroidSandookDriverFactory(private val context: Context) : SandookDriverFactory {
