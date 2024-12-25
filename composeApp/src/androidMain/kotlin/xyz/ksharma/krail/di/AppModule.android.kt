271976cca778585a7609a526c0e454d51d1a8617
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-25 12:20:40 +1100

    Update koin to 4.0.1

--- composeApp/src/androidMain/kotlin/xyz/ksharma/krail/di/AppModule.android.kt
+++ composeApp/src/androidMain/kotlin/xyz/ksharma/krail/di/AppModule.android.kt
@@ -2,10 +2,10 @@ package xyz.ksharma.krail.di
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.dsl.koinConfiguration
+import org.koin.core.KoinApplication
 import xyz.ksharma.krail.KrailApplication
 
-actual fun nativeConfig() = koinConfiguration {
+actual fun nativeConfig(): KoinApplication.() -> Unit = {
     androidLogger()
     androidContext(KrailApplication.instance ?: error("No Android application context set"))
 }
