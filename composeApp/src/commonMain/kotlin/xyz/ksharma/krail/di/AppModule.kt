f3f627c9b3a30a10f2c88680b80e45c566de6fff
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-28 12:19:26 +1100

    Add remoteConfigModule

--- composeApp/src/commonMain/kotlin/xyz/ksharma/krail/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/xyz/ksharma/krail/di/AppModule.kt
@@ -2,8 +2,29 @@ package xyz.ksharma.krail.di
 
 import org.koin.core.KoinApplication
 import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.includes
+import org.koin.dsl.koinConfiguration
 import org.koin.dsl.module
 import xyz.ksharma.krail.splash.SplashViewModel
+import xyz.ksharma.krail.core.analytics.di.analyticsModule
+import xyz.ksharma.krail.core.appinfo.di.appInfoModule
+import xyz.ksharma.krail.core.remote_config.di.remoteConfigModule
+import xyz.ksharma.krail.sandook.di.sandookModule
+import xyz.ksharma.krail.trip.planner.network.api.di.networkModule
+import xyz.ksharma.krail.trip.planner.ui.di.viewModelsModule
+
+val koinConfig = koinConfiguration {
+    includes(nativeConfig())
+    modules(
+        networkModule,
+        viewModelsModule,
+        sandookModule,
+        splashModule,
+        appInfoModule,
+        analyticsModule,
+        remoteConfigModule,
+    )
+}
 
 val splashModule = module {
     viewModelOf(::SplashViewModel)
