12562150604dfd0cb58e0eb48f5fa65f3f8f2897
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-12-28 13:06:54 +1100

    Revert "DI: Refactor Koin configuration and update to 4.0.1 (#484)"
    This reverts commit 116d5896e6248259aaf27fb9a0ea09b018af496b.

--- composeApp/src/androidMain/kotlin/xyz/ksharma/krail/di/AppModule.android.kt
+++ composeApp/src/androidMain/kotlin/xyz/ksharma/krail/di/AppModule.android.kt
@@ -2,10 +2,10 @@ package xyz.ksharma.krail.di
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.KoinApplication
+import org.koin.dsl.koinConfiguration
 import xyz.ksharma.krail.KrailApplication
 
-actual fun nativeConfig(): KoinApplication.() -> Unit = {
+actual fun nativeConfig() = koinConfiguration {
     androidLogger()
     androidContext(KrailApplication.instance ?: error("No Android application context set"))
 }
--- composeApp/src/commonMain/kotlin/xyz/ksharma/krail/KrailApp.kt
+++ composeApp/src/commonMain/kotlin/xyz/ksharma/krail/KrailApp.kt
@@ -2,33 +2,12 @@ package xyz.ksharma.krail
 
 import androidx.compose.runtime.Composable
 import org.koin.compose.KoinApplication
-import org.koin.dsl.includes
-import xyz.ksharma.krail.core.analytics.di.analyticsModule
-import xyz.ksharma.krail.core.appinfo.di.appInfoModule
-import xyz.ksharma.krail.core.remote_config.di.remoteConfigModule
-import xyz.ksharma.krail.di.nativeConfig
-import xyz.ksharma.krail.di.splashModule
-import xyz.ksharma.krail.sandook.di.sandookModule
+import xyz.ksharma.krail.di.koinConfig
 import xyz.ksharma.krail.taj.theme.KrailTheme
-import xyz.ksharma.krail.trip.planner.network.api.di.networkModule
-import xyz.ksharma.krail.trip.planner.ui.di.viewModelsModule
 
 @Composable
 fun KrailApp() {
-    KoinApplication(
-        application = {
-            includes(nativeConfig())
-            modules(
-                networkModule,
-                viewModelsModule,
-                sandookModule,
-                splashModule,
-                appInfoModule,
-                analyticsModule,
-                remoteConfigModule,
-            )
-        },
-    ) {
+    KoinApplication(application = koinConfig) {
         KrailTheme {
             KrailNavHost()
         }
