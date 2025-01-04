10b666051969f03b22c51e3eeefd9765b11cf3a8
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-01-05 09:44:27 +1100

    Provide android context to Koin slightly differently

--- composeApp/src/commonMain/kotlin/xyz/ksharma/krail/KrailApp.kt
+++ composeApp/src/commonMain/kotlin/xyz/ksharma/krail/KrailApp.kt
@@ -2,12 +2,16 @@ package xyz.ksharma.krail
 
 import androidx.compose.runtime.Composable
 import org.koin.compose.KoinApplication
+import org.koin.dsl.KoinAppDeclaration
 import xyz.ksharma.krail.di.koinConfig
 import xyz.ksharma.krail.taj.theme.KrailTheme
 
 @Composable
-fun KrailApp() {
-    KoinApplication(application = koinConfig) {
+fun KrailApp(koinDeclaration: KoinAppDeclaration? = null) {
+    KoinApplication(application = {
+        koinDeclaration?.invoke(this)
+        koinConfig()
+    }) {
         KrailTheme {
             KrailNavHost()
         }
