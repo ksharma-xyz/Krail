19828920104214f869ceb0af7a1e0d1c4d1ee221
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-01-05 15:09:44 +1100

    ADD UT for analytics events

--- core/test/src/commonTest/kotlin/xyz/ksharma/core/test/viewmodels/SavedTripViewModelsTest.kt
+++ core/test/src/commonTest/kotlin/xyz/ksharma/core/test/viewmodels/SavedTripViewModelsTest.kt
@@ -3,7 +3,6 @@ package xyz.ksharma.core.test.viewmodels
 import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
@@ -12,6 +11,7 @@ import kotlinx.coroutines.test.setMain
 import xyz.ksharma.core.test.fakes.FakeAnalytics
 import xyz.ksharma.core.test.fakes.FakeSandook
 import xyz.ksharma.krail.core.analytics.Analytics
+import xyz.ksharma.krail.core.analytics.event.AnalyticsEvent
 import xyz.ksharma.krail.sandook.Sandook
 import xyz.ksharma.krail.trip.planner.ui.savedtrips.SavedTripsViewModel
 import xyz.ksharma.krail.trip.planner.ui.state.savedtrip.SavedTripUiEvent
@@ -23,8 +23,6 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
-import kotlin.time.Duration.Companion.minutes
-import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class SavedTripsViewModelTest {
@@ -47,31 +45,33 @@ class SavedTripsViewModelTest {
     }
 
     @Test
-    fun `GIVEN initial state WHEN observer is active THEN analytics event should be tracked`() = runTest {
-        // Ensure analytics events have not been tracked before observation
-        assertFalse((analytics as FakeAnalytics).wasScreenViewEventTracked("view_screen"))
+    fun `GIVEN initial state WHEN observer is active THEN analytics event should be tracked`() =
+        runTest {
+            // Ensure analytics events have not been tracked before observation
+            assertFalse((analytics as FakeAnalytics).isEventTracked("view_screen"))
 
-        viewModel.uiState.test {
-            val item = awaitItem()
-            assertEquals(item, SavedTripsState())
+            viewModel.uiState.test {
+                val item = awaitItem()
+                assertEquals(item, SavedTripsState())
 
-            advanceUntilIdle()
-            assertTrue(analytics.wasScreenViewEventTracked("view_screen"))
+                advanceUntilIdle()
+                assertTrue(analytics.isEventTracked("view_screen"))
 
-            cancelAndConsumeRemainingEvents()
+                cancelAndConsumeRemainingEvents()
+            }
         }
-    }
 
     @Test
-    fun `GIVEN no observer is active WHEN checking analytics THEN event should not be tracked`() = runTest {
-        // GIVEN no observer is active
+    fun `GIVEN no observer is active WHEN checking analytics THEN event should not be tracked`() =
+        runTest {
+            // GIVEN no observer is active
 
-        // WHEN checking analytics
-        val eventTracked = (analytics as FakeAnalytics).wasScreenViewEventTracked("view_screen")
+            // WHEN checking analytics
+            val eventTracked = (analytics as FakeAnalytics).isEventTracked("view_screen")
 
-        // THEN event should not be tracked
-        assertFalse(eventTracked)
-    }
+            // THEN event should not be tracked
+            assertFalse(eventTracked)
+        }
 
     @Test
     fun `GIVEN a saved trip WHEN LoadSavedTrips event is triggered THEN UiState should update savedTrips`() =
@@ -137,4 +137,85 @@ class SavedTripsViewModelTest {
                 cancelAndIgnoreRemainingEvents()
             }
         }
+
+    @Test
+    fun `GIVEN a trip WHEN AnalyticsSavedTripCardClick event is triggered THEN trackSavedTripCardClick should be called`() =
+        runTest {
+            // GIVEN a trip
+            val fromStopId = "FROM_STOP_ID_1"
+            val toStopId = "TO_STOP_ID_1"
+
+            // WHEN the AnalyticsSavedTripCardClick event is triggered
+            viewModel.onEvent(SavedTripUiEvent.AnalyticsSavedTripCardClick(fromStopId, toStopId))
+
+            // THEN verify that [SavedTripCardClickEvent] is called with correct parameters
+            val fakeAnalytics: FakeAnalytics = analytics as FakeAnalytics
+            val eventName = AnalyticsEvent.SavedTripCardClickEvent(
+                fromStopId = fromStopId,
+                toStopId = toStopId
+            ).name
+            assertTrue(fakeAnalytics.isEventTracked(eventName))
+        }
+
+    @Test
+    fun `GIVEN a trip WHEN AnalyticsReverseSavedTrip event is triggered THEN track ReverseStopClickEvent should be called`() = runTest {
+        // WHEN the AnalyticsReverseSavedTrip event is triggered
+        viewModel.onEvent(SavedTripUiEvent.AnalyticsReverseSavedTrip)
+
+        // THEN verify that track is called with ReverseStopClickEvent
+        val fakeAnalytics: FakeAnalytics = analytics as FakeAnalytics
+        val eventName = AnalyticsEvent.ReverseStopClickEvent.name
+        assertTrue(fakeAnalytics.isEventTracked(eventName))
+    }
+
+    @Test
+    fun `GIVEN a trip WHEN AnalyticsLoadTimeTableClick event is triggered THEN trackLoadTimeTableClick should be called`() = runTest {
+        // GIVEN a trip
+        val fromStopId = "FROM_STOP_ID_1"
+        val toStopId = "TO_STOP_ID_1"
+
+        // WHEN the AnalyticsLoadTimeTableClick event is triggered
+        viewModel.onEvent(SavedTripUiEvent.AnalyticsLoadTimeTableClick(fromStopId, toStopId))
+
+        // THEN verify that trackLoadTimeTableClick is called with correct parameters
+        val fakeAnalytics: FakeAnalytics = analytics as FakeAnalytics
+        val eventName = AnalyticsEvent.LoadTimeTableClickEvent(
+            fromStopId = fromStopId,
+            toStopId = toStopId
+        ).name
+        assertTrue(fakeAnalytics.isEventTracked(eventName))
+    }
+
+    @Test
+    fun `GIVEN a trip WHEN AnalyticsSettingsButtonClick event is triggered THEN track SettingsClickEvent should be called`() = runTest {
+        // WHEN the AnalyticsSettingsButtonClick event is triggered
+        viewModel.onEvent(SavedTripUiEvent.AnalyticsSettingsButtonClick)
+
+        // THEN verify that track is called with SettingsClickEvent
+        val fakeAnalytics: FakeAnalytics = analytics as FakeAnalytics
+        val eventName = AnalyticsEvent.SettingsClickEvent.name
+        assertTrue(fakeAnalytics.isEventTracked(eventName))
+    }
+
+    @Test
+    fun `GIVEN a trip WHEN AnalyticsFromButtonClick event is triggered THEN track FromFieldClickEvent should be called`() = runTest {
+        // WHEN the AnalyticsFromButtonClick event is triggered
+        viewModel.onEvent(SavedTripUiEvent.AnalyticsFromButtonClick)
+
+        // THEN verify that track is called with FromFieldClickEvent
+        val fakeAnalytics: FakeAnalytics = analytics as FakeAnalytics
+        val eventName = AnalyticsEvent.FromFieldClickEvent.name
+        assertTrue(fakeAnalytics.isEventTracked(eventName))
+    }
+
+    @Test
+    fun `GIVEN a trip WHEN AnalyticsToButtonClick event is triggered THEN track ToFieldClickEvent should be called`() = runTest {
+        // WHEN the AnalyticsToButtonClick event is triggered
+        viewModel.onEvent(SavedTripUiEvent.AnalyticsToButtonClick)
+
+        // THEN verify that track is called with ToFieldClickEvent
+        val fakeAnalytics: FakeAnalytics = analytics as FakeAnalytics
+        val eventName = AnalyticsEvent.ToFieldClickEvent.name
+        assertTrue(fakeAnalytics.isEventTracked(eventName))
+    }
 }
