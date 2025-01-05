77d8448c74e235aca3491e4eaba972a150b52aa8
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-01-05 16:17:36 +1100

    Add UT for LoadTimeTable

--- core/test/src/commonTest/kotlin/xyz/ksharma/core/test/fakes/FakeTripPlanningService.kt
+++ core/test/src/commonTest/kotlin/xyz/ksharma/core/test/fakes/FakeTripPlanningService.kt
@@ -1,12 +1,18 @@
 package xyz.ksharma.core.test.fakes
 
+import xyz.ksharma.core.test.fakes.FakeTripResponseBuilder.buildDestinationStopSequence
+import xyz.ksharma.core.test.fakes.FakeTripResponseBuilder.buildOriginStopSequence
+import xyz.ksharma.core.test.fakes.FakeTripResponseBuilder.buildTransportation
 import xyz.ksharma.krail.trip.planner.network.api.model.StopFinderResponse
 import xyz.ksharma.krail.trip.planner.network.api.model.StopType
 import xyz.ksharma.krail.trip.planner.network.api.model.TripResponse
+import xyz.ksharma.krail.trip.planner.network.api.model.TripResponse.StopSequence
 import xyz.ksharma.krail.trip.planner.network.api.service.DepArr
 import xyz.ksharma.krail.trip.planner.network.api.service.TripPlanningService
 
 class FakeTripPlanningService : TripPlanningService {
+
+    var isSuccess: Boolean = true
 
     override suspend fun trip(
         originStopId: String,
@@ -15,9 +21,27 @@ class FakeTripPlanningService : TripPlanningService {
         date: String?,
         time: String?,
     ): TripResponse {
+        return if (isSuccess)
+
         // Return a fake TripResponse
-        return TripResponse(
-        )
+            TripResponse(
+                journeys = listOf(
+                    TripResponse.Journey(
+                        legs = listOf(
+                            TripResponse.Leg(
+                                origin = buildOriginStopSequence(),
+                                destination = buildDestinationStopSequence(),
+                                stopSequence = listOf(
+                                    buildOriginStopSequence(),
+                                ),
+                                transportation = buildTransportation(),
+                                duration = 100,
+                            ),
+                        ),
+                    ),
+                )
+            )
+        else throw IllegalStateException("Failed to fetch trip")
     }
 
     override suspend fun stopFinder(
