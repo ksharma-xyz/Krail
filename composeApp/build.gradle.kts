a5a0e869ace713dbf4dbb50223657c75efa05659
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-15 13:49:18 +1100

    Update workflow to run when the prod branch is cut off.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,7 +5,7 @@ android {
 
     defaultConfig {
         applicationId = "xyz.ksharma.krail"
-        versionCode = 46
+        versionCode = 47
         versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
