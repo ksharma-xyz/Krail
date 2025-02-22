338d1010157550618c3c2ac123c0492946ce82de
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-22 11:23:20 +1100

    Deps: Remove unused ktor-client-cio dependency (#624)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -119,7 +119,6 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtime.compose)
 
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.cio)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.logging)
             implementation(libs.ktor.serialization.kotlinx.json)
