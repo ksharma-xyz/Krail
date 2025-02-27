75b52b9a63597dc0a2df4e545af44af1830b2a7e
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-28 00:21:37 +1100

    Add RemoteConfig and ProtoPArser to AppStart

--- core/app-start/src/commonMain/kotlin/xyz/ksharma/krail/core/appstart/AppStart.kt
+++ core/app-start/src/commonMain/kotlin/xyz/ksharma/krail/core/appstart/AppStart.kt
@@ -1,5 +1,12 @@
 package xyz.ksharma.krail.core.appstart
 
 interface AppStart {
+
+    /**
+     * Called when the app starts.
+     * Perform all events here that should happen at app launch here.
+     *
+     * Only those operations should be launched here that should be scoped to application level.
+     */
     fun start()
 }
