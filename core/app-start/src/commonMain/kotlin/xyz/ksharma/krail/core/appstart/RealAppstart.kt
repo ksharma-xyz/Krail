40ba0ad33611f4a775682a9be85bc82dca8b8bc4
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-28 00:47:30 +1100

    Insert nsw stops if needed to be inserted

--- core/app-start/src/commonMain/kotlin/xyz/ksharma/krail/core/appstart/RealAppstart.kt
+++ core/app-start/src/commonMain/kotlin/xyz/ksharma/krail/core/appstart/RealAppstart.kt
@@ -5,25 +5,40 @@ import kotlinx.coroutines.launch
 import xyz.ksharma.krail.core.log.log
 import xyz.ksharma.krail.core.remote_config.RemoteConfig
 import xyz.ksharma.krail.io.gtfs.nswstops.ProtoParser
+import xyz.ksharma.krail.sandook.Sandook
 
 class RealAppStart(
     private val coroutineScope: CoroutineScope,
     private val remoteConfig: RemoteConfig,
     private val protoParser: ProtoParser,
+    private val sandook: Sandook,
 ) : AppStart {
     override fun start() {
         coroutineScope.launch {
 
             remoteConfig.setup() // App Start Event
 
-            kotlin.runCatching {
-                log("Reading NSW_STOPS proto file - StopsParser:")
-                // Call only if stops are not inserted in the database.
-                protoParser.parseAndInsertStops()
-            }.getOrElse {
-                log("Error reading proto file: $it")
-                // TODO - Firebase performance track.
-            }
+            parseAndInsertNswStopsIfNeeded()
         }
+    }
+
+    /**
+     * Parses and inserts NSW_STOPS data in the database if they are not already inserted.
+     */
+    private suspend fun parseAndInsertNswStopsIfNeeded() {
+        kotlin.runCatching {
+            if (isStopsNotInserted()) {
+                protoParser.parseAndInsertStops()
+            } else {
+                log("Stops already inserted in the database.")
+            }
+        }.getOrElse {
+            log("Error reading proto file: $it")
+            // TODO - Firebase performance track.
+        }
+    }
+
+    private fun isStopsNotInserted(): Boolean {
+        return sandook.stopsCount() == 0 || sandook.productClassCount() == 0
     }
 }
