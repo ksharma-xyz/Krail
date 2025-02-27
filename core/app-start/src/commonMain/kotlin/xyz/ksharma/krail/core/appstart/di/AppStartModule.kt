97b84045ad683ae042502df2e960d9c686f03833
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-28 00:14:13 +1100

    Create AppStart module

---
+++ core/app-start/src/commonMain/kotlin/xyz/ksharma/krail/core/appstart/di/AppStartModule.kt
@@ -0,0 +1,16 @@
+package xyz.ksharma.krail.core.appstart.di
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.koin.dsl.module
+import xyz.ksharma.krail.core.appstart.AppStart
+import xyz.ksharma.krail.core.appstart.RealAppStart
+
+val appStartModule = module {
+    single<AppStart> {
+        RealAppStart(
+            coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+        )
+    }
+}
