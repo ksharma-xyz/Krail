ae26a043b02bdfc596f1f5f878a79e5ebeb14a0e
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-28 17:28:38 +1100

    Add json and number type of flagvalues. Add ogic for getting defult list of  highlightStopIdList

--- core/remote-config/src/commonMain/kotlin/xyz/ksharma/krail/core/remote_config/flag/FlagKeys.kt
+++ core/remote-config/src/commonMain/kotlin/xyz/ksharma/krail/core/remote_config/flag/FlagKeys.kt
@@ -1,5 +1,6 @@
 package xyz.ksharma.krail.core.remote_config.flag
 
 enum class FlagKeys(val key: String) {
-    LOCAL_STOPS_ENABLED("local_stops_enabled")
+    LOCAL_STOPS_ENABLED("local_stops_enabled"),
+    HIGH_PRIORITY_STOP_IDS("high_priority_stop_ids"),
 }
--- core/remote-config/src/commonMain/kotlin/xyz/ksharma/krail/core/remote_config/flag/RemoteConfigFlag.kt
+++ core/remote-config/src/commonMain/kotlin/xyz/ksharma/krail/core/remote_config/flag/RemoteConfigFlag.kt
@@ -2,6 +2,7 @@ package xyz.ksharma.krail.core.remote_config.flag
 
 import xyz.ksharma.krail.core.log.log
 import xyz.ksharma.krail.core.remote_config.RemoteConfig
+import xyz.ksharma.krail.core.remote_config.RemoteConfigValue
 
 internal class RemoteConfigFlag(private val remoteConfig: RemoteConfig) : Flag {
 
@@ -9,9 +10,34 @@ internal class RemoteConfigFlag(private val remoteConfig: RemoteConfig) : Flag {
         val configValue = remoteConfig.getConfigValue(key)
         log("RemoteConfigFlag: getFlagValue: key=$key, configValue=$configValue")
 
-        return when (configValue.asString().toBooleanStrictOrNull()) {
-            true, false -> FlagValue.BooleanValue(configValue.asBoolean())
+        return when {
+            // Boolean
+            configValue.isBoolean() -> {
+                FlagValue.BooleanValue(configValue.asBoolean())
+            }
+
+            // Json
+            configValue.isJson() -> {
+                FlagValue.JsonValue(configValue.asString())
+            }
+
+            // Integer
+            configValue.isNumber() -> {
+                FlagValue.NumberValue(configValue.asNumber())
+            }
+
+            // String otherwise
             else -> FlagValue.StringValue(configValue.asString())
         }
     }
+
+    private fun RemoteConfigValue.isBoolean(): Boolean =
+        asString().toBooleanStrictOrNull() != null
+
+    private fun RemoteConfigValue.isJson(): Boolean =
+        asString().startsWith("[") || asString().startsWith("{")
+
+    private fun RemoteConfigValue.isNumber(): Boolean = asString().toLongOrNull() != null
+
+    private fun RemoteConfigValue.asNumber(): Long = asLong()
 }
