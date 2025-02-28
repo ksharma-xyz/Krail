70cfbbbd9f65dba2aebf9dce8feec600f03f7cbf
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-28 17:13:17 +1100

    IO: Add GTFS protobuf module and Wire plugin (#628)

---
+++ IO/gtfs/build.gradle.kts
@@ -0,0 +1,70 @@
+android {
+    namespace = "xyz.ksharma.krail.io.gtfs"
+}
+
+plugins {
+    alias(libs.plugins.krail.android.library)
+    alias(libs.plugins.krail.kotlin.multiplatform)
+    alias(libs.plugins.krail.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.wire)
+}
+
+kotlin {
+    applyDefaultHierarchyTemplate()
+
+    androidTarget()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        androidMain.dependencies {
+            api(libs.di.koinAndroid)
+        }
+
+        commonMain {
+            dependencies {
+                implementation(projects.core.log)
+                implementation(projects.core.di)
+
+                implementation(libs.kotlinx.serialization.json)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.auth)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.client.logging)
+                implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.kotlinx.datetime)
+                implementation(compose.runtime)
+
+                api(libs.di.koinComposeViewmodel)
+            }
+        }
+
+        iosMain {
+            dependencies {
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.test.kotlin)
+                implementation(libs.test.turbine)
+                implementation(libs.test.kotlinxCoroutineTest)
+            }
+        }
+    }
+}
+
+
+wire {
+    kotlin {
+        javaInterop = true
+        out = "$projectDir/build/generated/source/wire"
+    }
+    protoPath {
+        srcDir("src/commonMain/proto")
+    }
+    sourcePath {
+        srcDir("src/commonMain/proto")
+    }
+}
